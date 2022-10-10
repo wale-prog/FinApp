c93f0ac131a8051180f65859c40e9ccd60ed94ce
Olawale <olapetanwale@outlook.com>
2022-10-10 23:22:56 +0100

    Implement athentication

--- spec/model/category_spec.rb
+++ spec/model/category_spec.rb
@@ -1,27 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-
-  before (:all) do 
+  before(:all) do
     @user = User.create(name: 'John')
     @category = @user.categories.create(name: 'Rent', icon: 'fas fa-home')
   end
 
-  it "is valid with valid attributes" do
+  it 'is valid with valid attributes' do
     expect(@category).to be_valid
   end
 
-  it "is not valid without a name" do
+  it 'is not valid without a name' do
     @category.name = nil
     expect(@category).to_not be_valid
   end
 
-  it "is not valid without an icon" do
+  it 'is not valid without an icon' do
     @category.icon = nil
     expect(@category).to_not be_valid
   end
 
-  it "is not valid without a user" do
+  it 'is not valid without a user' do
     @category.user_id = nil
     expect(@category).to_not be_valid
   end
